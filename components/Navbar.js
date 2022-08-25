494c0fef949cdf0dd1c738c264a2819fdd216dac
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-08-25 11:29:46 +0100

    changed about to visit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from "next/link"
 import styles from "../styles/Home.module.css"
-import About from './../pages/about';
+import Visit from './../pages/visit';
 
 
 function Navbar() {
--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-// import express from "express"
-// import next from "next"
+//  import express from "express"
+// // import next from "next"
+
+// const app = express()
+
+// app.listen(3000, () => {
+//     console.log("app listening on port 3000")
+// })
 
 
+// app.get("/castledb", (req, res) => {
+//     res.json({
+//         message: "success"
+//     })
+// })
 
-// const port = process.env.PORT || 3001
+
+// const port = process.env.PORT || 3000
 // const dev = process.env.NODE_ENV !== 'production'
 // const app = next({ dev })
 // const handle = app.getRequestHandler()
@@ -17,6 +29,6 @@
 
 //   server.listen(port, () => {
 //     console.log(`> Ready on http://localhost:${port}`)
-//   })
- // "express": "nodemon ../nextjs-first/pages/_app",
-// })
+//   });
+//  "express": "nodemon ../nextjs-first/pages/_app",
+// });
